7c2e943af4acc57acbcb68931bed1ef0cd6007cb
Whoplaros <whoplaros@gmail.com>
2024-09-23 11:25:19 +0300

    added more js
    integrating the API, stopped at icon, time not right

--- src/app.js
+++ src/app.js
@@ -2,8 +2,38 @@ function refreshWeather(response) {
 	let temperatureElement = document.querySelector("#temperature");
 	let temperature = response.data.temperature.current;
 	let cityElement = document.querySelector("#city");
-	cityElement.innerHTML = response.data.city;
+	let countryElement = document.querySelector("#country");
+	let timeElement = document.querySelector("#time");
+	let descriptionElement = document.querySelector("#description");
+	let humidityElement = document.querySelector("#humidity");
+	let windspeedElement = document.querySelector("#windspeed");
+	let date = new Date(response.data.time * 1000);
+
 	temperatureElement.innerHTML = Math.round(temperature);
+	cityElement.innerHTML = response.data.city;
+	countryElement.innerHTML = response.data.country;
+	timeElement.innerHTML = formatDate(date);
+	descriptionElement.innerHTML = response.data.condition.description;
+	humidityElement.innerHTML = response.data.temperature.humidity;
+	windspeedElement.innerHTML = response.data.wind.speed;
+}
+function formatDate(date) {
+	let minutes = date.getMinutes();
+	let hours = date.getHours();
+	let days = [
+		"Sunday",
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+		"Saturday",
+	];
+	let day = days[date.getDay()];
+	if (minutes < 10) {
+		minutes = `0${minutes}`;
+	}
+	return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
