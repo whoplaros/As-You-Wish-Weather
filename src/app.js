981437740a61ae2527be05b406c0a822d849014d
Whoplaros <whoplaros@gmail.com>
2024-09-23 10:46:02 +0300

    added js
    start js with temp refresh function and loading auto with nicosia

--- src/app.js
+++ src/app.js
@@ -1,22 +1,25 @@
 function refreshWeather(response) {
-	console.log(response.data);
+	let temperatureElement = document.querySelector("#temperature");
+	let temperature = response.data.temperature.current;
+	let cityElement = document.querySelector("#city");
+	cityElement.innerHTML = response.data.city;
+	temperatureElement.innerHTML = Math.round(temperature);
 }
 
 function searchCity(city) {
-	//make api call and update the interface
-
 	let apiKey = "e546fd9teb385774339o4d3b5b79c3a0";
-	let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+	let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 	axios.get(apiUrl).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
 	event.preventDefault();
 	let searchInput = document.querySelector("#search-form-input");
-	let cityElement = document.querySelector("#city");
-	cityElement.innerHTML = searchInput.value;
+
 	searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Nicosia");
