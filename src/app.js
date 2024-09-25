85ccb25d7ac4d346c6d0b3a1d5294ddda7b2ca57
Whoplaros <whoplaros@gmail.com>
2024-09-25 07:58:57 +0300

    inject forecast HTML from JS

--- src/app.js
+++ src/app.js
@@ -10,7 +10,6 @@ function refreshWeather(response) {
 	let date = new Date(response.data.time * 1000);
 	let icon = response.data.condition.icon_url;
 	let currentIcon = document.querySelector("#icon");
-	console.log(response.data.condition.icon_url);
 	currentIcon.src = `${icon}`;
 
 	temperatureElement.innerHTML = Math.round(temperature);
@@ -53,7 +52,32 @@ function handleSearchSubmit(event) {
 	searchCity(searchInput.value);
 }
 
+function displayForecast() {
+	let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+	let forecastHtml = "";
+
+	days.forEach(function (day) {
+		forecastHtml =
+			forecastHtml +
+			`
+		<div class="weather-forecast-day">
+			<div class="weather-forecast-date">${day}</div>
+			<div class="weather-forecast-icon">🌤️</div>
+			<div class="weather-forecast-temperatures">
+				<div class="weather-forecast-temperature">
+				<strong>15° </strong>
+				</div>
+				<div class="weather-forecast-temperature">9°</div>
+			</div>
+		</div>
+		`;
+	});
+	let forecastElement = document.querySelector("#forecast");
+	forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Nicosia");
+displayForecast();
