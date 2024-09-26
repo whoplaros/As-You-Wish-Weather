891833e997dc0706ec1996d43d5335b35ca68a2e
Whoplaros <whoplaros@gmail.com>
2024-09-26 11:29:42 +0300

    adding get and display Forecast functions

--- src/app.js
+++ src/app.js
@@ -19,6 +19,7 @@ function refreshWeather(response) {
 	descriptionElement.innerHTML = response.data.condition.description;
 	humidityElement.innerHTML = response.data.temperature.humidity;
 	windspeedElement.innerHTML = response.data.wind.speed;
+	getForecast(response.data.city);
 }
 function formatDate(date) {
 	let minutes = date.getMinutes();
@@ -52,7 +53,14 @@ function handleSearchSubmit(event) {
 	searchCity(searchInput.value);
 }
 
-function displayForecast() {
+function getForecast(city) {
+	let apiKey = "e546fd9teb385774339o4d3b5b79c3a0";
+	let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+	axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+	console.log(response.data);
 	let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
 	let forecastHtml = "";
 
@@ -80,4 +88,3 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Nicosia");
-displayForecast();
